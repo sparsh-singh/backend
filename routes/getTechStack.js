f56fb89843c718f740af54e8d95f0bf94d8b5427
Sparsh Kumar Singh <spasingh@cisco.com>
2023-05-28 14:09:55 +0530

    made some chnages lib

--- routes/getTechStack.js
+++ routes/getTechStack.js
@@ -1,23 +1,33 @@
-// const axios = require("axios")
-const request = require('request');
+ const axios = require("axios")
+// const request = require('request');
 require("dotenv").config()
 async function getTechStack(url, req , res) {
-    console.log(url)
-    var options = {
-        'method': 'GET',
-        'url': 'https://www.genelify.com/api/v1/technology-lookup?url='+url,
-        'headers': {
-          'X_GENELIFY_API_KEY': process.env.X_GENELIFY_API_KEY,
-        }
-      };
+    
+    const options = {
+        method: "GET",
+        url: "https://www.genelify.com/api/v1/technology-lookup",
 
-      
-      request(options, function (error, response) {
-        if (error) throw res.json(error);
-        res.send(response.body);
-      });    
+        params: { url: url },
+        headers: {
+            "Content-Type": "application/json",
+            "X-GENELIFY-API-KEY": "62a97a3153c136252e66aeead667fdd1c03b220e1839e3694811e66f9539c64d",
+        },
+    }
+    
+
+    try {
+        const response = await axios.request(options)
+        res.json(response.data)
+        // request(options, function (error, response) {
+        //     if (error) throw new Error(error);
+        //     console.log(response.data,error)
+        //     res.json(response.data)
+        //   });
 
 
+    } catch (error) {
+        res.json({"error": error})
+    }
 }
 
 module.exports = getTechStack
